db47d64c7c1aa6d1b2521b0e9eacbe5b08b633f1
Esraa Said <esraa195182@feng.bu.edu.eg>
2025-07-28 15:30:21 +0300

    session-5

--- session-5/movies_management_project/config/db.js
+++ session-5/movies_management_project/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://test:bRA6e6eeuMoXWNV7@cluster0.uoedbx4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+      "connection string",
       { dbName: "Movies" }
     );
     console.log(`Database Connection Successfully`);
